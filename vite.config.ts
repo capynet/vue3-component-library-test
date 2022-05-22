a978027264000892ceec7423ee4fdb8cdcd048ee
Marcelo Tosco <marcelo@feedbackcompany.com>
2022-05-22 11:30:16 +0200

    Testing async component library.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,26 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.js"),
+      name: "Components library",
+      fileName: (format) => `components.${format}.js`,
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 });
