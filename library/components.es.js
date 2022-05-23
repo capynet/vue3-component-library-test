fd934ea2e2cb1797e2c78afc6cfdc6e3996af33b
Marcelo <marcelo@feedbackcompany.com>
2022-05-23 14:21:30 +0200

    WIP

--- library/components.es.js
+++
@@ -1,4 +0,0 @@
-import { defineAsyncComponent } from "vue";
-const HelloWorld = defineAsyncComponent(() => import("./HelloWorld.js"));
-const CustomButton = defineAsyncComponent(() => import("./CustomButton.js"));
-export { CustomButton, HelloWorld };
