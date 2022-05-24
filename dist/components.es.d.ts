5bd1a12a4f529648b7b98c637f9b30da657d67ad
Marcelo <marcelo@feedbackcompany.com>
2022-05-24 08:07:18 +0200

    WIP

--- dist/components.es.d.ts
+++
@@ -1,3 +0,0 @@
-import components_library from './lib/main.js'
-export default components_library
-export * from './lib/main.js'
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -19,17 +13,13 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "lib/main.js"),
-      name: "components_library",
+      entry: path.resolve(__dirname, "src/components.ts"),
+      name: "ComponentsLibrary",
       fileName: (format) => `components.${format}.js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: "Vue",
         },
